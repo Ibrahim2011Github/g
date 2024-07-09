d3145263b9f6a339788df95df1139d7848962089
Ibrahim2011Github <ibragim.ajdynly@gmail.com>
2024-07-09 14:39:29 +0400

    first commit

---
+++ JavaScript/first.js
@@ -0,0 +1,6 @@
+var Name = "Ibrahim";
+var Age = 13
+var hobby = "to play chess"
+var school = "Young Talents"
+var birthday = "March 24th"
+console.log("My name is " + Name + ". I'm " + Age + ". My hobby is " + hobby + ". I study in " + school + ". My birthday is " + birthday)
